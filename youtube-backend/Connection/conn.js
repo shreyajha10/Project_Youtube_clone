0c8f7b3fe08095e886eef0fbaa713b3fd76c2366
Shreya Jha <writeshreyajha10@gmail.com>
2024-11-24 17:45:17 +0530

    Update backend

--- youtube-backend/Connection/conn.js
+++ youtube-backend/Connection/conn.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/youtube")
+mongoose.connect("mongodb://localhost:27017/clone-youtube")
 const db = mongoose.connection;
 db.on("open", () => {
   console.log("Connection is successfull ho gya.");
